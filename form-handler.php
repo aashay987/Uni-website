98a6ffdb3c56ef68c72264b9a7003de536cb4bcd
aashay987 <66820851+aashay987@users.noreply.github.com>
2021-06-09 12:22:06 +0530

    Added files

---
+++ form-handler.php
@@ -0,0 +1,19 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$message=$_POST['message'];
+
+$email_from='info@website.com';
+$subject="Site querry";
+$email_body= "Username : $name.\n"
+            "User Email : $visitor_email.\n"
+            "Subject : $subject.\n"
+            "User Email: $message.\n";
+
+$to ='aashaypatel9091@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers = "Reply to $visitor_email \r\n";
+mail($to,$subject,$email_body,$headers)
+
+header("Location: contact.html")
+ ?>
